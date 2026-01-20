b2790aa0987f189e4a0de56bcb3a8eef5d257ed1
Nouseback <nouseback@hotmail.fr>
2026-01-20 08:50:58 +0100

    Mise à jour du footer avec le nom John Doe

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="bg-dark text-light text-center p-3 mt-5">
-      <p className="mb-1">© 2026 Benjamin Szabo</p>
+      <p className="mb-1">© 2026 John Doe</p>
       <a href="/mentions-legales" className="text-light">
         Mentions légales
       </a>
